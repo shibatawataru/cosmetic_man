d4c44be84093e77a3ada4f6c527367b1fc980d1d
shibatawataru <watawatarinco1128@gmail.com>
2022-06-14 12:55:18 +0000

    編集画面途中

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   namespace :public do
     resources :items, only: [:new,:index,:show,:edit,:create,:update,:destroy]
        end
+
+  namespace :public do
+    resources :customers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
