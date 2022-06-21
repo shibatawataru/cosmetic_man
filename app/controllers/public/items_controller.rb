fa0972fe16eb71d0e0775488c6e34a83d6716b72
shibatawataru <watawatarinco1128@gmail.com>
2022-06-21 11:24:14 +0000

    新規投稿

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < ApplicationController
   def new
       @item = Item.new(params[:id])
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.customer_id = current_customer.id
@@ -18,24 +18,25 @@ class Public::ItemsController < ApplicationController
     render :new
     end
   end
-  
+
   def index
     @customer = current_customer
     @items = Item.all
     @quantity = Item.count
     @tag_list = Tag.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
     @customer = @item.customer
     @comment = Comment.new
+    @item_tags = @item.tags
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -45,15 +46,15 @@ class Public::ItemsController < ApplicationController
     render :edit
     end
   end
-  
+
   def destroy
     @item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to public_items_path
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:body, :itemname, :price, :product_image, :evaluation)
   end
