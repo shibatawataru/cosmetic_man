8f0aaa5af7c8d5447ff0cbec49739a7e0694f9df
shibatawataru <watawatarinco1128@gmail.com>
2022-07-19 09:55:48 +0000

    アイテムモデルインデント

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,20 @@
 class Item < ApplicationRecord
-    belongs_to :customer
-    has_many :item_tags,dependent: :destroy
-    has_many :tags,through: :item_tags
-    has_many :comments, dependent: :destroy
-    has_one_attached :product_image
-    
-    validates :itemname, :body, :price, :evaluation, presence: true
-    validates :price, numericality: true
+  belongs_to :customer
+  has_many :item_tags,dependent: :destroy
+  has_many :tags,through: :item_tags
+  has_many :comments, dependent: :destroy
+  has_one_attached :product_image
+  
+  validates :itemname, :body, :price, :evaluation, presence: true
+  validates :price, numericality: true
 
-    def get_product_image(width, height)
-      unless product_image.attached?
-        file_path = Rails.root.join('app/assets/images/no_image.jpeg')
-        product_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      end
-      product_image.variant(resize_to_limit: [width, height]).processed
+  def get_product_image(width, height)
+    unless product_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      product_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+    product_image.variant(resize_to_limit: [width, height]).processed
+  end
 
   def save_tag(sent_tags)
     # タグのIDをすべて配列で取得しcurrent_tagsとする
